0a4b2969b1e9767e55a691125ccc2a6bbb0635e5
payor <nikoleta.pajor@gmail.com>
2013-12-28 03:26:53 +0100

    Rozdzial 1

--- bin/printpdf.sh
+++ bin/printpdf.sh
@@ -2,4 +2,6 @@
 
 rm -rfv ./target/*
 pdflatex --output-directory=./target/ bsc.tex
+pdflatex --output-directory=./target/ bsc.tex ## twice because of TOC
+
 mv ./target/bsc.pdf ./target/.tmp && rm -rfv ./target/* && mv ./target/.tmp ./target/bsc.pdf
